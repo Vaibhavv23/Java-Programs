c8ad4451acddd77694ff2a372f00cf7fa296bbe1
Vaibhavv23 <iamvaibhav2000@gmail.com>
2020-02-02 00:36:34 +0530

    added code

--- Control Statement Programs/src/Factorial.java
+++ Control Statement Programs/src/Factorial.java
@@ -1,10 +1,44 @@
+//Program to Find the Factorial of the Number Using increment operator
+
+import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Factorial of = ");
+		int a = input.nextInt();
+		int fact = 1;
 		
+		for(int i = 1; i <= a; i++) { 
+			fact = fact * i;
+			System.out.println(fact);
+		}
 	}
-
 }
+
+
+/*
+//Program to Find the Factorial of the Number Using decrement operator
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Factorial of = ");
+		int a = input.nextInt();
+		int fact = 1;
+		
+		for(int i = a; i >= 1; i--) { 
+			fact = fact * i;
+			System.out.println(fact);
+		}
+	}
+}
+*/
