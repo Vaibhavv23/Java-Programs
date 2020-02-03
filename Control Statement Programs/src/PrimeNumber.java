b05e3e77a0b0966ea06df9462f40fa9546fd15fe
Vaibhavv23 <iamvaibhav2000@gmail.com>
2020-02-03 23:41:12 +0530

    new program added

---
+++ Control Statement Programs/src/PrimeNumber.java
@@ -0,0 +1,29 @@
+//Program to Check Whether A Number is Prime or Not
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number to check whether prime or not = ");
+		int num = input.nextInt();
+		int temp = 0;
+		
+		for(int i = 2; i <= num-1; i++) {
+			if(num % i == 0) {
+				temp = temp + 1;
+			}
+		}
+		if(temp > 0) {
+			System.out.println("Not a Prime number");
+		}else {
+			System.out.println("It is a Prime number");
+		}
+	}
+}
+
+//It is more easy to find number which is not a prime than a prime.
+//so the above technique is done in that way.
