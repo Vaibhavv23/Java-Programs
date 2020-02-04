9ef3c79436a8c5830a229d2c0df828baa3c34975
Vaibhavv23 <iamvaibhav2000@gmail.com>
2020-02-05 01:34:56 +0530

    new commit

---
+++ Pattern Programs/src/StarPattern1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StarPattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new  Scanner(System.in);
+		System.out.println("Enter any number below 10 = ");
+		int num = input.nextInt();
+		System.out.println("## Star Pattern loading... ##");
+		System.out.println();
+		
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+}
